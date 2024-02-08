65a39c2640ece377c937c260d0117954968d99f4
NatalNW7 <natanaelweslley1@gmail.com>
2024-02-08 15:46:20 -0300

    read pdf inter

--- fatura.py
+++ fatura.py
@@ -1,6 +1,8 @@
 import tabula
 import PyPDF2
 import pandas as pd
+import re
+import json
 
 
 def get_total_pages(pdf: str) -> int:
@@ -13,14 +15,14 @@ def get_total_pages(pdf: str) -> int:
 
 def read_pdf(pdf, pages, header=None) -> pd.DataFrame:
     csv_output = "output.csv"
-
+    
     tabula.convert_into(pdf, csv_output, output_format="csv", pages=pages)
     fatura = pd.read_csv(csv_output, names=header, delimiter=',')
 
     return fatura
 
 def read_nubank(pdf) -> pd.DataFrame:
-    header=['Data', 'Unnamed', 'Movimentação', 'Valor']
+    header=['Data', 'Unnamed', 'Movimentacao', 'Valor']
     pages = f'4-{get_total_pages(pdf)}'
     fatura = read_pdf(pdf, pages, header)
     fatura.drop(columns=['Unnamed'], inplace=True)
@@ -28,9 +30,28 @@ def read_nubank(pdf) -> pd.DataFrame:
 
     return fatura
 
-def read_inter(pdf): #-> pd.DataFrame:
-    # fatura = tabula.read_pdf(pdf, pages=2, lattice=True)
+def read_inter(pdf):
     fatura = PyPDF2.PdfReader(pdf)
+    text = fatura.pages[1].extract_text()
+    dict_fatura = []
+    with open('output.txt', 'w') as fatura_txt:
+        fatura_txt.write(text)
+
+    with open('output.txt', 'r') as fatura_txt:
+        text = fatura_txt.readlines()
+
+        for line in text:
+            cleaned_line = re.sub(r'\xa0|\x00', ' ', line.strip())
+            extracted = re.search(r'^(\d{2}\s\w{3}\s\d{4})(.+)(R\$\s\d.\d{1,},\d{2}|R\$\s\d{1,},\d{2})$', cleaned_line)
+
+            if extracted:
+                dict_fatura.append({
+                    'Data': extracted.group(1).strip(),
+                    'Movimentacao': extracted.group(2).strip(),
+                    'Valor': extracted.group(3).strip()
+                })
+
+    fatura = pd.DataFrame(dict_fatura)
 
     return fatura
 
@@ -40,30 +61,12 @@ if '__main__' == __name__:
     pdf_meliuz = 'meliuz-2023-07.pdf'
     pdf_pan = 'pan_2023-07.pdf'
 
-    # fatura_nubank = read_nubank(pdf_nubank)
+    fatura_nubank = read_nubank(pdf_nubank)
+    print(fatura_nubank)
     fatura_inter = read_inter(pdf_inter)
+    print(fatura_inter)
 
-    text = fatura_inter.pages[1].extract_text()
-    # print(fatura_inter.pages[1].extract_text())
-    print(text)
-    with open('teste.txt', 'w') as fatura:
-        fatura.write(text)
-
-    import re
-
-    with open('teste.txt', 'r') as fatura:
-        text = fatura.readlines()
-        print(text)
-        for line in text:
-            cleaned_line = re.sub(r'\xa0|\x00', ' ', line.strip())
-            extracted = re.search(r'^(\d{2}\s\w{3}\s\d{4})(.+)(R\$\s\d.\d{1,},\d{2}|R\$\s\d{1,},\d{2})$', cleaned_line)
-            if extracted:
-                print(extracted.groups())
-                print('data =', extracted.group(1))
-                print('movimentação =', extracted.group(2))
-                print('valor =', extracted.group(3))
-
-    # TODO terminar funcão de leitura do pdf inter
+    # TODO fazer leitura de pdf pan
 
             
     
