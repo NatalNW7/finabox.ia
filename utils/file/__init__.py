6bc31c8479f44e891ad82e16f5f4237836f35274
NatalNW7 <natanaelweslley1@gmail.com>
2024-04-13 13:52:19 -0300

    change parameters

--- utils/file/__init__.py
+++ utils/file/__init__.py
@@ -2,13 +2,13 @@ from json import loads
 from utils import PathConstants
 
 
-def writer(content, file: str = PathConstants.OUTPUT_TXT):
-    with open(file, 'w') as file:
+def writer(content, file_path: str = PathConstants.OUTPUT_TXT):
+    with open(file_path, 'w') as file:
         characters_written = file.write(content)
     
     return True if characters_written > 0 else False
 
-def reader(file: str = PathConstants.OUTPUT_TXT):
+def reader(file: str = PathConstants.OUTPUT_TXT) -> (dict | list[str]):
     with open(file, 'r') as content:
         if '.json' in file:
             return loads(content.read())
@@ -20,7 +20,7 @@ def reader(file: str = PathConstants.OUTPUT_TXT):
     return lines
 
 def deleter(file: str = PathConstants.OUTPUT_TXT):
-    if not writer('content was deleted', file=file):
+    if not writer('content was deleted', file_path=file):
         raise Exception('Error to delete content')
     
     return True
